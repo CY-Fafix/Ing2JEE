2f8c2de955181138fe09d1a289cb819997f2bc7b
CY-Fafix <ProtonPingouin@protonmail.com>
2023-10-26 19:25:07 +0200

    Correction erreur session

--- src/main/java/fr/cytech/recette/Controlleur_Recette.java
+++ src/main/java/fr/cytech/recette/Controlleur_Recette.java
@@ -4,6 +4,8 @@ import fr.cytech.ingredient.Ingredient;
 import fr.cytech.ingredient.Ingredient_Repository;
 import fr.cytech.ingredientRecette.IngredientRecette;
 import fr.cytech.ingredientRecette.IngredientRecette_Repository;
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
