5609d5feedb9eac882a8ee0ee6ddcfff4ea1d2ca
CY-Fafix <ProtonPingouin@protonmail.com>
2023-10-27 13:12:01 +0200

    Possibilité de rajouter un ingrédient deja existant

--- src/main/java/fr/cytech/recette/Controlleur_Recette.java
+++ src/main/java/fr/cytech/recette/Controlleur_Recette.java
@@ -7,6 +7,9 @@ import fr.cytech.ingredientRecette.IngredientRecette_Repository;
 import fr.cytech.utilisateur.*;
 import jakarta.servlet.http.HttpSession;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,25 +35,36 @@ public class Controlleur_Recette {
 
 
     @GetMapping(path="/ajouterRecette")
-    public String ajouterRecetteForm(Model model,HttpSession session) {
-    	if( session.getAttribute("id_utilisateur") != null ) {
-    		model.addAttribute("recette", new Recette());
-            // je dois changer d'autres données genre la liste d'ingrédients
-            return "ajouterRecette";  // le nom du fichier HTML de la page de création
-    	} else {
-			 model.addAttribute("erreur", "Merci de se connecter pour ajouter une recette");
-    		return "connexion";
-    	}
+    public String ajouterRecetteForm(Model model, HttpSession session) {
+        if( session.getAttribute("id_utilisateur") != null ) {
+            model.addAttribute("recette", new Recette());
+            List<Ingredient> ingredients = ingredientRepository.findAll();
+            model.addAttribute("ingredients", ingredients);
+            return "ajouterRecette";
+        } else {
+            model.addAttribute("erreur", "Merci de se connecter pour ajouter une recette");
+            return "connexion";
+        }
     }
+
 
     @PostMapping("/ajouter_recette")
     public String ajouterRecetteSubmit(@ModelAttribute Recette recette,
                                        @RequestParam(required = false) String[] nom_ingredient,
                                        @RequestParam(required = false) Double[] quantite,
                                        @RequestParam(required = false) String[] unite,
+                                       @RequestParam(required = false) Double[] quantiteExistant,
+                                       @RequestParam(required = false) String[] uniteExistant,
+                                       @RequestParam(required = false) List<Long> ingredientsExistantsIds,
+
                                        HttpSession session) {
-        System.out.println("Soumission du formulaire de recette");
-        
+        System.out.println("Les id : " + ingredientsExistantsIds);
+        System.out.println("Quantite: " + Arrays.toString(quantiteExistant));
+        System.out.println("Unite: " + Arrays.toString(uniteExistant));
+
+        if(uniteExistant == null) {
+            System.out.println("Pas de quantité");
+        }
         // Récupérer l'utilisateur à partir de la session
         Long utilisateurId = (Long) session.getAttribute("id_utilisateur");
         if(utilisateurId == null) {
@@ -68,14 +82,32 @@ public class Controlleur_Recette {
         // Enregistrement de la recette
         Recette savedRecette = recetteRepository.save(recette);
 
-        // Traitement des ingrédients
-        for (int i = 0; i < nom_ingredient.length; i++) {
-            String ingredientNom = nom_ingredient[i];
-            Ingredient ingredient = ingredientRepository.findByNomIngredient(ingredientNom)
-                    .orElseGet(() -> ingredientRepository.save(new Ingredient(ingredientNom)));
-
-            IngredientRecette ingredientRecette = new IngredientRecette(ingredient, savedRecette, quantite[i], unite[i]);
-            ingredientRecetteRepository.save(ingredientRecette);
+        // Traitement des ingrédients existants
+        if (ingredientsExistantsIds != null && quantiteExistant != null && uniteExistant != null) {
+            for (int i = 0; i < ingredientsExistantsIds.size(); i++) {
+                Long ingredientId = ingredientsExistantsIds.get(i);
+                Optional<Ingredient> optionalIngredient = ingredientRepository.findById(ingredientId);
+                if (optionalIngredient.isPresent()) {
+                    Ingredient ingredient = optionalIngredient.get();
+                    Double ingredientQuantite = quantiteExistant[i];
+                    String ingredientUnite = uniteExistant[i];
+                    IngredientRecette ingredientRecette = new IngredientRecette(ingredient, savedRecette, ingredientQuantite, ingredientUnite);
+                    ingredientRecetteRepository.save(ingredientRecette);
+                }
+            }
+        }
+        
+        // Traitement des nouveaux ingrédients
+        if (nom_ingredient != null) {
+            for (int i = 0; i < nom_ingredient.length; i++) {
+                String ingredientNom = nom_ingredient[i];
+                Ingredient ingredient = ingredientRepository.findByNomIngredient(ingredientNom)
+                        .orElseGet(() -> ingredientRepository.save(new Ingredient(ingredientNom)));
+                Double ingredientQuantite = quantite != null && quantite.length > i ? quantite[i] : null;
+                String ingredientUnite = unite != null && unite.length > i ? unite[i] : null;
+                IngredientRecette ingredientRecette = new IngredientRecette(ingredient, savedRecette, ingredientQuantite, ingredientUnite);
+                ingredientRecetteRepository.save(ingredientRecette);
+            }
         }
 
         return "redirect:/index";
--- src/main/resources/static/js/recette.js
+++ src/main/resources/static/js/recette.js
@@ -1,24 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
     var ingredientsList = document.getElementById('ingredientsList');
     var ajouterIngredientBtn = document.getElementById('ajouterIngredient');
+    var ingredientsExistantsSelect = $('#ingredientsExistant');
     
+    // Initialisation de Select2 pour le champ de sélection des ingrédients existants
+    ingredientsExistantsSelect.select2();
+    
+    // Gestion de l'ajout d'un ingrédient manuellement
     ajouterIngredientBtn.addEventListener('click', function() {
-        // On récupère le dernier groupe d'ingrédients
         var dernierIngredient = ingredientsList.lastElementChild;
-        
-        // On clone le dernier groupe d'ingrédients
         var nouvelIngredient = dernierIngredient.cloneNode(true);
 
-        // On met à jour les noms pour les rendre uniques et interprétables comme des tableaux
         var inputs = nouvelIngredient.querySelectorAll('input, select');
         inputs.forEach(function(input) {
             if(input.name) {
                 input.name = input.name.replace(/\[\d+\]/, '[' + (ingredientsList.childElementCount - 1) + ']');
             }
-            input.value = ''; // Réinitialise la valeur
+            input.value = '';
         });
 
-        // Ajoute le nouveau groupe d'ingrédients à la liste
         ingredientsList.appendChild(nouvelIngredient);
+    });
+    
+    // Gestion de la sélection d'un ingrédient existant
+    ingredientsExistantsSelect.on('select2:select', function (e) {
+		    console.log('Ingrédient sélectionné', e.params.data);
+        var data = e.params.data;
+        var ingredientId = data.id;
+        var ingredientNom = data.text;
+        
+		var html = `
+		    <div class="ingredient-existant-detail" data-ingredient-id="${ingredientId}">
+		        <!-- Ajout d'un champ caché pour l'ID de l'ingrédient -->
+		        <input type="hidden" name="ingredientsExistantsIds" value="${ingredientId}">
+		        
+		        <h4>${ingredientNom}</h4>
+		        <div class="form-group">
+		            <label for="quantiteExistant_${ingredientId}">Quantité:</label>
+		            <input type="number" id="quantiteExistant_${ingredientId}" name="quantiteExistant" required>
+		        </div>
+		        <div class="form-group">
+		            <label for="uniteExistant_${ingredientId}">Unité:</label>
+		            <select id="uniteExistant_${ingredientId}" name="uniteExistant">
+		                <option value="grammes">Grammes</option>
+		                <option value="litres">Litres</option>
+		                <option value="cuillères à soupe">Cuillères à soupe</option>
+		            </select>
+		        </div>
+		    </div>
+		`;
+		document.getElementById('ingredientsExistantsDetails').insertAdjacentHTML('beforeend', html);
+    });
+
+    // Gestion de la désélection d'un ingrédient existant
+    ingredientsExistantsSelect.on('select2:unselect', function (e) {
+        var data = e.params.data;
+        var ingredientId = data.id;
+        document.querySelector(`.ingredient-existant-detail[data-ingredient-id="${ingredientId}"]`).remove();
     });
 });
