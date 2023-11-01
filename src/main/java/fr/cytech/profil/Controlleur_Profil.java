bd27949a77a237061013843eaa5d6033adcff865
Diego <diegojedachercoles@gmail.com>
2023-11-01 12:40:01 +0100

    Possibilité de modifier user, mail et mdp depuis modifier profil et fixé le pb de la date nulle. TODO: css de modifier profil

--- src/main/java/fr/cytech/profil/Controlleur_Profil.java
+++ src/main/java/fr/cytech/profil/Controlleur_Profil.java
@@ -1,7 +1,8 @@
 package fr.cytech.profil;
 
-import java.sql.Date;
-import java.util.Optional;
+
+
+import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.cytech.utilisateur.Utilisateur;
 import fr.cytech.utilisateur.Utilisateur_Repository;
@@ -21,6 +23,8 @@ public class Controlleur_Profil {
 	
 	@Autowired
 	Utilisateur_Repository utilisateur_repository;
+	
+
 	
 	@GetMapping(path="/creerProfil")
 	public String creerProfil(Model model,HttpSession session ) {
@@ -39,11 +43,14 @@ public class Controlleur_Profil {
 			Profil profil=profil_repository.findByUtilisateur(utilisateur);
 			model.addAttribute("profil",profil);
 			
+			model.addAttribute("utilisateurConnecte",utilisateur);
+			
 			if(profil==null) {
 				return "creerProfil";
 			}else {
 				return "gererProfil";
-			}		
+			}
+			
 		
 		}catch (java.lang.NullPointerException e) {
 			//cas ou l'utilisateur n'est pas connecté
@@ -61,6 +68,7 @@ public class Controlleur_Profil {
 			Utilisateur utilisateur=utilisateur_repository.findById(id);
 			Profil profil=profil_repository.findByUtilisateur(utilisateur);
 			model.addAttribute("AncienProfil", profil);
+			model.addAttribute("utilisateurConnecte",utilisateur);
 			return "editerProfil";
 		}else {
 			model.addAttribute("erreur", "Merci de vous connecter pour acceder à cette page");
@@ -81,20 +89,43 @@ public class Controlleur_Profil {
 	}
 	
 	@PostMapping(path="Modifier_Profil")
-	public String Modifier_Profil(Model model,@ModelAttribute Profil profil,HttpSession session) {
+	public String Modifier_Profil(Model model,@RequestParam("bio") String bio,@RequestParam("dateDeNaissance") String str_date,@RequestParam("nom") String nom, @RequestParam("email") String email, @RequestParam("mdp") String mdp, HttpSession session) {
 		try {
+			Profil profil=new Profil();
+			
+			try {
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				java.util.Date javaUtilDate = sdf.parse(str_date);
+				java.sql.Date dateNaissance = new java.sql.Date(javaUtilDate.getTime());
+				profil.setDateDeNaissance(dateNaissance);
+								
+				
+			}catch (Exception e) {
+				java.sql.Date dateNaissance=null;
+				profil.setDateDeNaissance(dateNaissance);				
+			}
+			profil.setBio(bio);
+			
+			
+			
 			long id=(long)session.getAttribute("id_utilisateur");
 			Utilisateur utilisateur=utilisateur_repository.findById(id);
 			Profil profilExistant=profil_repository.findByUtilisateur(utilisateur);
-			boolean modifierdate=true;
-			profil.getDateDeNaissance();
 
 			if(profil.getBio()!=null) {
 				profilExistant.setBio(profil.getBio());
 			}
-			if(modifierdate) {
-				profilExistant.setDateDeNaissance(profil.getDateDeNaissance());
+			
+			if(nom!=null) {
+				utilisateur.setNom(nom);
 			}
+			if(email!=null) {
+				utilisateur.setMail(email);
+			}
+			if(mdp!=null) {
+				utilisateur.setMdp(mdp);
+			}
+			utilisateur_repository.save(utilisateur);
 			profil_repository.save(profilExistant);
 			return "index";
 
@@ -106,6 +137,7 @@ public class Controlleur_Profil {
 		
 		
 	}
+	
 	
 	
 }
--- src/main/java/fr/cytech/profil/Profil_Repository.java
+++ src/main/java/fr/cytech/profil/Profil_Repository.java
@@ -1,9 +1,6 @@
 package fr.cytech.profil;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import fr.cytech.utilisateur.Utilisateur;
 
