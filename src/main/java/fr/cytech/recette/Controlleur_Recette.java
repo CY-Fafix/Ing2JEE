8b1738d116d5cd755d445387149987e12bb91e9c
CY-Fafix <ProtonPingouin@protonmail.com>
2023-11-03 23:12:57 +0100

    corrections model et controlleur nouvelle recette

--- src/main/java/fr/cytech/recette/Controlleur_Recette.java
+++ src/main/java/fr/cytech/recette/Controlleur_Recette.java
@@ -66,31 +66,38 @@ public class Controlleur_Recette {
             System.out.println("Pas de quantité");
         }
         // Récupérer l'utilisateur à partir de la session
-        Long utilisateurId = (Long) session.getAttribute("id_utilisateur");
+        Long utilisateurId = (Long) session.getAttribute("id_utilisateur");;
+        //Si pas connecté on le redirige
         if(utilisateurId == null) {
             return "redirect:/connexion";
         }
+        //on cherche l'utilisateur dans la bDD
         Optional<Utilisateur> optionalUtilisateur = utilisateurRepository.findById(utilisateurId);
+        // Si il existe pas on le redirige 
         if(!optionalUtilisateur.isPresent()) {
             return "redirect:/connexion"; 
         }
         Utilisateur utilisateur = optionalUtilisateur.get();
-        if(utilisateur == null) {
-            return "redirect:/connexion";
-        }
         recette.setAuteur(utilisateur); 
         // Enregistrement de la recette
         Recette savedRecette = recetteRepository.save(recette);
 
         // Traitement des ingrédients existants
         if (ingredientsExistantsIds != null && quantiteExistant != null && uniteExistant != null) {
+        	//On parcours les ingrédients
             for (int i = 0; i < ingredientsExistantsIds.size(); i++) {
+            	//on récupère l'id
                 Long ingredientId = ingredientsExistantsIds.get(i);
+                //On recupere l'ingredient dans la BDD
                 Optional<Ingredient> optionalIngredient = ingredientRepository.findById(ingredientId);
+                //Si l'ingrédient est dans la BDD
                 if (optionalIngredient.isPresent()) {
+                	//on recup l'ingrédient
                     Ingredient ingredient = optionalIngredient.get();
+                    //on récupère la quantité et l'unité
                     Double ingredientQuantite = quantiteExistant[i];
                     String ingredientUnite = uniteExistant[i];
+                    //On ajoute tout à la BDD
                     IngredientRecette ingredientRecette = new IngredientRecette(ingredient, savedRecette, ingredientQuantite, ingredientUnite);
                     ingredientRecetteRepository.save(ingredientRecette);
                 }
@@ -98,17 +105,31 @@ public class Controlleur_Recette {
         }
         
         // Traitement des nouveaux ingrédients
+     // Traitement des nouveaux ingrédients
         if (nom_ingredient != null) {
             for (int i = 0; i < nom_ingredient.length; i++) {
                 String ingredientNom = nom_ingredient[i];
                 Ingredient ingredient = ingredientRepository.findByNomIngredient(ingredientNom)
                         .orElseGet(() -> ingredientRepository.save(new Ingredient(ingredientNom)));
-                Double ingredientQuantite = quantite != null && quantite.length > i ? quantite[i] : null;
-                String ingredientUnite = unite != null && unite.length > i ? unite[i] : null;
+                
+                Double ingredientQuantite = null;
+                // On check si il y a bien une quantité et on dépasse pas le tableau
+                if (quantite != null && quantite.length > i) {
+                    ingredientQuantite = quantite[i];
+                }
+                
+                // On check si il y a bien une unité et on dépasse pas le tableua
+                String ingredientUnite = null;
+                if (unite != null && unite.length > i) {
+                    ingredientUnite = unite[i];
+                }
+                
+                // Création et sauvegarde de l'ingrédient de la recette
                 IngredientRecette ingredientRecette = new IngredientRecette(ingredient, savedRecette, ingredientQuantite, ingredientUnite);
                 ingredientRecetteRepository.save(ingredientRecette);
             }
         }
+
 
         return "redirect:/index";
     }
--- src/main/resources/static/js/recette.js
+++ src/main/resources/static/js/recette.js
@@ -1,3 +1,9 @@
+/* doc :https://select2.org/
+Fonction : 
+Permet de dupliquer les formulaires de création d'ingrédients,
+Permet de changer les valeurs des forms pour le controlleur
+*/
+
 document.addEventListener('DOMContentLoaded', function() {
     var ingredientsList = document.getElementById('ingredientsList');
     var ajouterIngredientBtn = document.getElementById('ajouterIngredient');
@@ -6,32 +12,35 @@ document.addEventListener('DOMContentLoaded', function() {
     // Initialisation de Select2 pour le champ de sélection des ingrédients existants
     ingredientsExistantsSelect.select2();
     
-    // Gestion de l'ajout d'un ingrédient manuellement
+    // Ajout d'un ingrédient manuel 
     ajouterIngredientBtn.addEventListener('click', function() {
+		//on recupere le dernier ingredient
         var dernierIngredient = ingredientsList.lastElementChild;
+        //on le clone
         var nouvelIngredient = dernierIngredient.cloneNode(true);
-
+        
+        //On recupere tous les inpus et select
         var inputs = nouvelIngredient.querySelectorAll('input, select');
+        //On parcourt tous les input du formulaire
         inputs.forEach(function(input) {
-            if(input.name) {
-                input.name = input.name.replace(/\[\d+\]/, '[' + (ingredientsList.childElementCount - 1) + ']');
-            }
+            //Les valeurs sont remises à zero
             input.value = '';
         });
-
+        
+        //On affiche le nouveau formulaire
         ingredientsList.appendChild(nouvelIngredient);
     });
     
-    // Gestion de la sélection d'un ingrédient existant
+    // Ajout d'un ingrédient existant
+    //On séléctionne un ingrédient dans le menu select2, place l'id dans le hidden pour relier ingrédient et recette
     ingredientsExistantsSelect.on('select2:select', function (e) {
-		    console.log('Ingrédient sélectionné', e.params.data);
         var data = e.params.data;
         var ingredientId = data.id;
         var ingredientNom = data.text;
         
+        //Formulaire pour ajouter la quantité et l'unité de l'ingrédient
 		var html = `
 		    <div class="ingredient-existant-detail" data-ingredient-id="${ingredientId}">
-		        <!-- Ajout d'un champ caché pour l'ID de l'ingrédient -->
 		        <input type="hidden" name="ingredientsExistantsIds" value="${ingredientId}">
 		        
 		        <h4>${ingredientNom}</h4>
@@ -49,6 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		        </div>
 		    </div>
 		`;
+		//On insère la div a la suite des formulaires deja existants
 		document.getElementById('ingredientsExistantsDetails').insertAdjacentHTML('beforeend', html);
     });
 
